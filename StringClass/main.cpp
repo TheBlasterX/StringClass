c9af26a1ea838a2cf94f31544ded9bbb4d9b1ce6
Stefan Jipa <17stefanjipa@gmail.com>
2023-05-04 18:29:49 +0300

    Initial Commit

---
+++ StringClass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StringClass
+//
+//  Created by Ștefan Jipa on 04.05.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
